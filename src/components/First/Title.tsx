34c6b49a76405d51b04036226925c60fcaffddc3
Jhonatec <jhonsreis@gmail.com>
2023-10-23 21:21:08 -0300

    feat: first content

--- src/components/First/Title.tsx
+++ src/components/First/Title.tsx
@@ -1,7 +1,5 @@
 export default function Title() {
   return (
-    <h1 className="">
-      NORTE DO ARQUITETO
-    </h1>
+    
   )
 }
--- src/components/First/index.tsx
+++ src/components/First/index.tsx
@@ -1,9 +1,23 @@
-import Title from "./Title";
+import ananda from '@/assets/images/ananda.jpeg';
+import logo from '@/assets/images/logo.png';
+import Image from "next/image";
 
 export default function First() {
   return (
-    <div>
-      <Title />
+    <div className="flex justify-between items-center gap-4 max-w-md">
+      <Image src={ananda} alt="ananda" width={500} className='ananda-no-color' />
+      <div>
+        <h1 className="max-w-m white-space-normal mb-">
+          NORTE DO ARQUITETO
+        </h1>
+        <div className="flex justify-between items-center  gap-4">
+          <div className="flex flex-col gap-2">
+            <h3 className='max-w-md'>PORTFÓLIO DE IMAGENS REALISTAS 3D</h3>
+            <button className="btn-primary text-white font-bold py-2 px-2 rounded-full">Veja mais</button>
+          </div>
+          <Image src={logo} alt="logo" width={150} />
+        </div>
+      </div>
     </div>
   )
 }
