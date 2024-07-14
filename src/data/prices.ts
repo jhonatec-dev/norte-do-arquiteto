802b0e67a676891be28ec8f0b05f674db4e81194
JhonaTec <jhonsreis@gmail.com>
2024-07-13 23:01:36 -0300

    data: new prices

--- src/data/prices.ts
+++ src/data/prices.ts
@@ -1,47 +1,47 @@
 export const prices = [
   {
     title: "Lavabo",
-    render: 65,
-    modelagem: 100,
-  },
-  {
-    title: "Banheiro",
     render: 75,
-    modelagem: 130,
-  },
-  {
-    title: "Quarto",
-    render: 75,
-    modelagem: 150,
-  },
-  {
-    title: "Sala",
-    render: 65,
-    modelagem: 150,
-  },
-  {
-    title: "Cozinha",
-    render: 75,
-    modelagem: 180,
-  },
-  {
-    title: "Área de Serviço",
-    render: 65,
-    modelagem: 100,
-  },
-  {
-    title: "Área Gourmet",
-    render: 75,
-    modelagem: 180,
-  },
-  {
-    title: "Área Gourmet com piscina",
-    render: 85,
     modelagem: 200,
   },
   {
+    title: "Banheiro",
+    render: 85,
+    modelagem: 230,
+  },
+  {
+    title: "Quarto",
+    render: 95,
+    modelagem: 300,
+  },
+  {
+    title: "Sala",
+    render: 95,
+    modelagem: 300,
+  },
+  {
+    title: "Cozinha",
+    render: 100,
+    modelagem: 300,
+  },
+  {
+    title: "Área de Serviço",
+    render: 85,
+    modelagem: 150,
+  },
+  {
+    title: "Área Gourmet",
+    render: 100,
+    modelagem: 350,
+  },
+  {
+    title: "Área Gourmet com piscina",
+    render: 130,
+    modelagem: 400,
+  },
+  {
     title: "Consultório",
-    render: 75,
-    modelagem: 180,
+    render: 100,
+    modelagem: 300,
   },
 ];
